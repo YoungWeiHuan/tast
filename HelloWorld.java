40d1128a5e649aadae6bb724f1c25f9e25b29b40
ywh <13427274035@163.com>
2020-11-05 16:36:14 +0800

    HelloWorld modified

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,5 +4,18 @@ public class HelloWorld {
 		System.out.println("1");
 		System.out.println("HelloWorld!");
 		System.out.println("2");
+
+		int num = 0;
+		for (int i = 0; i < 35; i++) {
+			for (int j = 0; j < 35; j++) {
+				if (i * 2 + j * 4 == 94 & i + j == 35) {
+					System.out.println("鸡"+i+"只，兔"+j+"只");
+					num = 1;
+				}
+			}
+		}
+		if (num == 0){
+			System.out.println("无解");
+		}
 	}
 }
